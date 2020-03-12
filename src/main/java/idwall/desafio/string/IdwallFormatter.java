bfa3968b18ead2581b568473de023a07df0409d5
LittleGuava <f.nunes05@outlook.com>
2020-03-12 15:15:18 -0300

    Initiliazing and editing .gitignore

---
+++ src/main/java/idwall/desafio/string/IdwallFormatter.java
@@ -0,0 +1,21 @@
+package idwall.desafio.string;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Created by Rodrigo Catão Araujo on 06/02/2018.
+ */
+public class IdwallFormatter extends StringFormatter {
+
+    /**
+     * Should format as described in the challenge
+     *
+     * @param text
+     * @return
+     */
+    @Override
+    public String format(String text) {
+        
+        return "aaa";
+    }
+}
