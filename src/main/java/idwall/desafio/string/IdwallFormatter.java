b5cdf6470d45bc8299c551a6c0782f41808c4fae
LittleGuava <f.nunes05@outlook.com>
2020-03-13 00:12:49 -0300

    Tentativa de justificado (bug de espaços em excesso no final)

--- src/main/java/idwall/desafio/string/IdwallFormatter.java
+++ src/main/java/idwall/desafio/string/IdwallFormatter.java
@@ -2,6 +2,9 @@ package idwall.desafio.string;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by Rodrigo Catão Araujo on 06/02/2018.
  */
@@ -9,6 +12,9 @@ public class IdwallFormatter extends StringFormatter {
 
     private static final Integer DEFAULT_NUMBER_OF_CHARS = 40;
     private static final Boolean DEFAULT_JUSTIFY = true;
+
+    private int limit = DEFAULT_NUMBER_OF_CHARS;
+    private boolean justified = DEFAULT_JUSTIFY;
 
     /**
      * Should format as described in the challenge
@@ -24,7 +30,11 @@ public class IdwallFormatter extends StringFormatter {
             //TODO MUDAR NOME DAS VARIAVEIS PARA INGLÊS
 
             for (String linhaASerFormatada : linhas) {
-                formattedText.append(processLines(linhaASerFormatada));
+                if (linhaASerFormatada.isEmpty()) {
+                    formattedText.append(StringUtils.LF);
+                } else {
+                    formattedText.append(processLines(linhaASerFormatada)).append(StringUtils.LF);
+                }
             }
 
         } else {
@@ -36,18 +46,55 @@ public class IdwallFormatter extends StringFormatter {
 
     private StringBuilder processLines(String linhaASerFormatada) {
         StringBuilder formattedLines = new StringBuilder();
-        String[] splittedWords = linhaASerFormatada.split(" ");
+        List<String> splittedWords = Arrays.asList(linhaASerFormatada.split(" "));
         StringBuilder aux = new StringBuilder();
 
-        for (String pacoca : splittedWords) {
-            if ((aux + StringUtils.SPACE + pacoca).length() > DEFAULT_NUMBER_OF_CHARS) {
-                formattedLines.append(aux).append(StringUtils.LF);
-                aux = new StringBuilder(pacoca);
-            } else if ((aux + pacoca).length() <= 40) {
-                aux.append(pacoca).append(StringUtils.SPACE);
+        for (String word : splittedWords) {
+
+            int tamanho = (aux + word).length();
+            if (splittedWords.get(splittedWords.size() - 1).equals(word)) {
+//                formattedLines.append(aux).append(word);
+                formattedLines.append(justificado(aux.toString()+word));
+                justificado(aux.toString());
+                aux = new StringBuilder();
+            } else if (tamanho == DEFAULT_NUMBER_OF_CHARS) {
+//                formattedLines.append(aux).append(word).append(StringUtils.LF);
+                formattedLines.append(justificado(aux.toString()+word)).append(StringUtils.LF);
+                System.out.println("Entrei aqui" + aux);
+                justificado(aux.toString());
+                aux = new StringBuilder();
+            } else if (tamanho > DEFAULT_NUMBER_OF_CHARS) {
+                System.out.println("Entrei aqui : ->>>>" + aux);
+                justificado(aux.toString());
+//                formattedLines.append(aux).append(StringUtils.LF);
+                formattedLines.append(justificado(aux.toString())).append(StringUtils.LF);
+                aux = new StringBuilder(word).append(StringUtils.SPACE);
+            } else if (tamanho < 40) {
+                aux.append(word).append(StringUtils.SPACE);
             }
         }
         return formattedLines;
+    }
+
+    public String justificado(String line) {
+        if (justified) {
+            if (line.length() == 40) {
+            } else {
+                int diferencaEntreTamanhoDaFraseEDoJustificado = limit - line.length();
+                for (int i = line.indexOf(' '); i < line.length(); i++) {
+                    if (line.charAt(i) == ' ' && diferencaEntreTamanhoDaFraseEDoJustificado > 0) {
+                        line = line.replace(" ", "  ");
+                        break;
+                    }
+                    if (i < line.length()-1 && diferencaEntreTamanhoDaFraseEDoJustificado > 0) {
+                        i = line.indexOf(' ');
+                    }
+                }
+                System.out.println(StringUtils.countMatches(line, " ") + "------------>" + diferencaEntreTamanhoDaFraseEDoJustificado);
+            }
+        }
+        System.out.println(line);
+        return line;
     }
 
     @Override
